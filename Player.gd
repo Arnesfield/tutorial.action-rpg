dc9e827333120e8ca27cb409912c918871272265
Jefferson Rylee <rylee.jeff385@gmail.com>
2020-03-28 21:14:40 +0800

    Add Player movement script

--- Player.gd
+++ Player.gd
@@ -1,16 +1,18 @@
 extends KinematicBody2D
 
+export var velocity: int = 140
+var direction: Vector2 = Vector2.ZERO
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _physics_process(delta: float):
+  movement_process(delta)
 
+func movement_process(delta: float):
+  var up = Input.is_action_pressed("ui_up")
+  var down = Input.is_action_pressed("ui_down")
+  var left = Input.is_action_pressed("ui_left")
+  var right = Input.is_action_pressed("ui_right")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-  pass # Replace with function body.
+  direction = Vector2(int(right) - int(left), int(down) - int(up))
+  var move: Vector2 = direction.normalized() * velocity * delta
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#  pass
+  move_and_collide(move)
