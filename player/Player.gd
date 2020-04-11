fd2f66a200ddcc27be3298b35870f8ea07d6f66b
Jefferson Rylee <rylee.jeff385@gmail.com>
2020-04-11 19:16:51 +0800

    Add animation tree for Player Animate Player when moving and idle Update Player position in World

--- player/Player.gd
+++ player/Player.gd
@@ -5,23 +5,33 @@ export var friction: float = 25
 export var max_speed: float = 175
 var velocity: Vector2 = Vector2.ZERO
 
-func _physics_process(delta: float):
-  movement_process(delta)
+onready var animationTree: AnimationTree = $AnimationTree
+onready var animationState: AnimationNodeStateMachine = animationTree.get("parameters/playback")
 
-func movement_process(delta: float):
-  var up = Input.is_action_pressed("ui_up")
-  var down = Input.is_action_pressed("ui_down")
-  var left = Input.is_action_pressed("ui_left")
-  var right = Input.is_action_pressed("ui_right")
+func _physics_process(_delta: float):
+  movement_process()
 
-  var direction: Vector2 = Vector2(
-    int(right) - int(left),
-    int(down) - int(up)
-  ).normalized()
+func movement_process():
+  var direction: Vector2 = get_input_direction().normalized()
 
-  if (direction != Vector2.ZERO):
+  if direction != Vector2.ZERO:
+    set_animation_direction(direction)
     velocity = velocity.move_toward(direction * max_speed, acceleration)
   else:
     velocity = velocity.move_toward(Vector2.ZERO, friction)
 
   velocity = move_and_slide(velocity)
+  animationState.travel("run" if velocity != Vector2.ZERO else "idle")
+
+func set_animation_direction(direction: Vector2):
+  if not is_inside_tree():
+    return
+  animationTree.set("parameters/idle/blend_position", direction)
+  animationTree.set("parameters/run/blend_position", direction)
+
+func get_input_direction() -> Vector2:
+  var up = int(Input.is_action_pressed("ui_up"))
+  var down = int(Input.is_action_pressed("ui_down"))
+  var left = int(Input.is_action_pressed("ui_left"))
+  var right = int(Input.is_action_pressed("ui_right"))
+  return Vector2(right - left, down - up)
